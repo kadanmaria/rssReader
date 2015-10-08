9fcd81640879d444005759a45828517be8fe5065
iStudent <kadan.maria@gmail.com>
2015-10-08 20:26:15 +0300

    First day commit

---
+++ rssReader/rssReader/MyFirstClass.swift
@@ -0,0 +1,9 @@
+//
+//  MyFirstClass.swift
+//  rssReader
+//
+//  Created by iStudent on 10/8/15.
+//  Copyright © 2015 iStudent. All rights reserved.
+//
+
+import Foundation
---
+++ rssReader/rssReader/MyFirstClassTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyFirstClassTableViewCell.swift
+//  rssReader
+//
+//  Created by iStudent on 10/8/15.
+//  Copyright © 2015 iStudent. All rights reserved.
+//
+
+import UIKit
+
+class MyFirstClassTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
