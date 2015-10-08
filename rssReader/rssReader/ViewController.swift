37f71255648b782fb411b907ac23c67a2ebb5bf7
iStudent <kadan.maria@gmail.com>
2015-10-08 18:33:08 +0300

    My first commit

---
+++ rssReader/rssReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rssReader
+//
+//  Created by iStudent on 10/8/15.
+//  Copyright © 2015 iStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
