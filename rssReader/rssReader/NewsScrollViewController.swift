49d7bd32ca1db9434862a2ad8d1d7f543d61597b
Admin <admin@Mac-Admin.local>
2015-12-09 00:12:56 +0200

    Add scroll view controller

---
+++ rssReader/rssReader/NewsScrollViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsScrollViewController.swift
+//  rssReader
+//
+//  Created by Admin on 08.12.15.
+//  Copyright © 2015 iStudent. All rights reserved.
+//
+
+import UIKit
+
+class NewsScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
